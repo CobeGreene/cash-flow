82e93745da3a473748901f459dd03649e85e3d64
Cobe Greene <cobegreene@gmail.com>
2026-02-17 21:06:30 -0800

    Add percentage change

--- user_interface/src/components/TableBreakdown.vue
+++ user_interface/src/components/TableBreakdown.vue
@@ -36,9 +36,14 @@ type RowHeader = {
 	category: string | null
 	subCategory: string | null
 }
+type RowValue = {
+	value: string
+	percentageChange: string
+	isPositive: boolean
+}
 
 const tableBreakdown = computed(() => {
-	const result = [] as (RowHeader | string)[][]
+	const result = [] as (RowHeader | RowValue)[][]
 	result.push([{ name: props.timeUnit, level: RowLevel.Top, category: null, subCategory: null }])
 	result.push([{ name: 'Income', level: RowLevel.Top, category: 'Income', subCategory: null }])
 	for (const subCategory of categories.value['Income'] || []) {
@@ -66,35 +71,74 @@ const tableBreakdown = computed(() => {
 	}
 
 	let startDate = props.getPreviousTime(props.getPreviousTime(props.currentDate))
+	let previousStartDate = props.getPreviousTime(startDate)
 	const endDate = props.getNextTime(props.getNextTime(props.currentDate))
 
 	while (startDate <= endDate) {
 		const transactionsInTime = transactions.value.filter((transaction) =>
 			props.isSameTimeAsCurrentDate(getDate(transaction), startDate)
 		)
-		result[0].push(props.getFormatTime(startDate))
+		const transactionsPreviousTime = transactions.value.filter((transaction) =>
+			props.isSameTimeAsCurrentDate(getDate(transaction), previousStartDate)
+		)
+		result[0].push({
+			value: props.getFormatTime(startDate),
+			isPositive: true,
+			percentageChange: '0',
+		})
 		for (let i = 1; i < result.length; i++) {
 			const rowHeader = result[i][0] as RowHeader
+			let inverse = -1
 			let sliceFunc = createSliceFunc(rowHeader.category, rowHeader.subCategory, true)
 			if (rowHeader.category == 'Income') {
+				inverse = 1
 				sliceFunc = createSliceFunc(rowHeader.category, rowHeader.subCategory, false)
+			} else if (rowHeader.category == 'Investment') {
+				inverse = 1
 			} else if (rowHeader.name == 'Expenses') {
 				sliceFunc = expensesSliceFunc
 			}
-			result[i].push(
+			const currentValue = totalBreakdownAmount(
+				transactionsInTime,
+				sliceFunc,
 				// eslint-disable-next-line @typescript-eslint/no-unused-vars
-				totalBreakdownAmount(transactionsInTime, sliceFunc, (_: Date) => true).toFixed(2)
+				(_: Date) => true
 			)
+			const previousValue = totalBreakdownAmount(
+				transactionsPreviousTime,
+				sliceFunc,
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				(_: Date) => true
+			)
+			let percentageChange = 0
+			if (previousValue !== 0) {
+				percentageChange = (currentValue - previousValue) / Math.abs(previousValue)
+			}
+			result[i].push({
+				value: currentValue.toFixed(2),
+				percentageChange: (percentageChange * 100).toFixed(2),
+				isPositive: percentageChange * inverse >= 0,
+			})
 		}
 
 		startDate = props.getNextTime(startDate)
+		previousStartDate = props.getNextTime(previousStartDate)
 	}
 
 	return result
 })
 
-function getRowHeader(row: (string | RowHeader)[]): RowHeader {
+function getRowHeader(row: (RowValue | RowHeader)[]): RowHeader {
 	return row[0] as RowHeader
+}
+function getRowValue(value: RowValue | RowHeader): string {
+	return (value as RowValue).value
+}
+function getRowPercentageChange(value: RowValue | RowHeader): string {
+	return (value as RowValue).percentageChange
+}
+function getRowValueIsPositive(value: RowValue | RowHeader): boolean {
+	return (value as RowValue).isPositive
 }
 </script>
 
@@ -104,7 +148,7 @@ function getRowHeader(row: (string | RowHeader)[]): RowHeader {
 			<tr>
 				<th scope="col">Category</th>
 				<th v-for="(header, index) in tableBreakdown[0].slice(1)" :key="index">
-					{{ header }}
+					{{ getRowValue(header) }}
 				</th>
 			</tr>
 		</thead>
@@ -124,8 +168,18 @@ function getRowHeader(row: (string | RowHeader)[]): RowHeader {
 				>
 					{{ getRowHeader(row).name }}
 				</td>
-				<td v-for="(value, index) in row.slice(1)" :key="index">${{ value }}</td>
+				<td v-for="(value, index) in row.slice(1)" :key="index">
+					${{ getRowValue(value) }}
+					<span
+						:class="{
+							'text-success': getRowValueIsPositive(value),
+							'text-danger': !getRowValueIsPositive(value),
+						}"
+						>( {{ getRowPercentageChange(value) }}%)</span
+					>
+				</td>
 			</tr>
 		</tbody>
 	</table>
 </template>
+
